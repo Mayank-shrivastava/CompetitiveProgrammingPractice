fbcd6927867bb9e6846dfccca9487dda2643c073
mayank <mayank.shrivastava_cs19@gla.ac.in>
2020-11-04 21:01:33 +0530

    update1

--- src/EnourmousInput.java
+++ src/EnourmousInput.java
@@ -5,58 +5,15 @@
  *   File: EnourmousInput.java
  */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-
 public class EnourmousInput {
     public static void main(String[] args) {
-        FastScanner fastScanner = new FastScanner();
-    }
-}
-
-class FastScanner {
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastScanner() {
-        br = new BufferedReader(new
-                InputStreamReader(System.in));
-    }
-
-    String next() {
-        while (st == null || !st.hasMoreElements()) {
-            try {
-                st = new StringTokenizer(br.readLine());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return st.nextToken();
-    }
-
-    int nextInt() {
-        return Integer.parseInt(next());
-    }
-
-    long nextLong() {
-        return Long.parseLong(next());
-    }
-
-    double nextDouble() {
-        return Double.parseDouble(next());
-    }
-
-    String nextLine() {
-        String str = "";
-        try {
-            str = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return str;
+        // swapping operation
+        int a = 4, b = 5;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + "," + b);
     }
 }
 
---
+++ src/hackerearth/basicprogramming/CostOfBalloons.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Mayank Shrivastava.
+ *   Date: 04-11-2020
+ *   Time: 20:46
+ *   File: CostOfBalloons.java
+ */
+
+package hackerearth.basicprogramming;
+
+import java.util.Scanner;
+
+public class CostOfBalloons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int cg = sc.nextInt(), cp = sc.nextInt();
+            int noOfParticipants = sc.nextInt();
+        }
+    }
+}
+
