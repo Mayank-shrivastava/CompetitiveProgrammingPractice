82cd3f9312bb43776005768c8560b37ce25f1888
mayank <mayank.shrivastava_cs19@gla.ac.in>
2020-11-04 22:20:47 +0530

    add solution to Cost of Balloons

--- src/hackerearth/basicprogramming/CostOfBalloons.java
+++ src/hackerearth/basicprogramming/CostOfBalloons.java
@@ -16,6 +16,20 @@ public class CostOfBalloons {
         while (t-- > 0) {
             int cg = sc.nextInt(), cp = sc.nextInt();
             int noOfParticipants = sc.nextInt();
+            int[][] participantsResults = new int[noOfParticipants][2];
+            for (int i = 0; i < noOfParticipants; i++) {
+                participantsResults[i][0] = sc.nextInt();
+                participantsResults[i][1] = sc.nextInt();
+            }
+            int totalFirstAns = 0, totalSecondAns = 0;
+            for (int i = 0; i < noOfParticipants; i++) {
+                if (participantsResults[i][0] == 1) totalFirstAns++;
+                if (participantsResults[i][1] == 1) totalSecondAns++;
+            }
+            int x = cp * totalFirstAns + cg * totalSecondAns;
+            int y = cp * totalSecondAns + cg * totalFirstAns;
+            if (x > y) System.out.println(y);
+            else System.out.println(x);
         }
     }
 }
